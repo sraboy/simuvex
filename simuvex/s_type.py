fc088e941e3fd9ee7dfc4b1a17ec915fa6d88b02
Steven Lavoie <steve.l@digitaloperatives.com>
2017-07-28 17:57:38 -0500

    Implement ssize in s_type

--- simuvex/s_type.py
+++ simuvex/s_type.py
@@ -718,8 +718,8 @@ ALL_TYPES = {
     'ptrdiff_t': SimTypeLong(False),
     'size_t': SimTypeLength(False),
     'ssize_t': SimTypeLength(True),
-    'uintptr_t' : SimTypeLong(False),
-
+    'ssize': SimTypeLength(False),
+    'uintptr_t': SimTypeLong(False),
     'string': SimTypeString(),
 }
 
